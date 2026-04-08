bc2c4567d3016d9a3fc574c39b9f296e46979c3d
Bekbolsun Akimov <akimovbekbolsun332@gmail.com>
2026-04-08 10:23:42 +0600

    Add Main cards

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -9,7 +9,33 @@ const Main = () => {
         <p>Gemini</p>
         <img src={assets.user_icon} alt="" />
       </div>
+      <div className="main-container">
+        <div className="greet">
+          <p>
+            <span>Hello, Dev.</span>
+          </p>
+          <p>How can I help you today?</p>
+        </div>
+        <div className="cards">
+          <div className="card">
+            <p>Suggest beautiful places to see on an upcoming road trip</p>
+            <img src={assets.compass_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Suggest beautiful places to see on an upcoming road trip</p>
+            <img src={assets.compass_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Suggest beautiful places to see on an upcoming road trip</p>
+            <img src={assets.compass_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Suggest beautiful places to see on an upcoming road trip</p>
+            <img src={assets.compass_icon} alt="" />
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 export default Main;
