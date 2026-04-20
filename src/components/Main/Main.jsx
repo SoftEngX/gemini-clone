8bef369a0ea87391e5223fc085de1a5d110f8ad2
Bekbolsun Akimov <akimovbekbolsun332@gmail.com>
2026-04-20 19:53:17 +0600

    --date=2026-04-08 18:12:56

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import './Main.css';
+import { assets } from '../assets/assets';
 
 const Main = () => {
   return (
-    <>
-      <div>I'm the main</div>
-    </>
+    <div className="main">
+      <div className="nav">
+        <p>Gemini</p>
+        <img src={assets.user_icon} alt="" />
+      </div>
+    </div>
   )
 }
 export default Main;
